80119d8a0cf708de40c842c621669cdd409a97e5
STANLEY <stanleyrubanza@gmail.com>
2022-09-21 11:33:23 +0300

    stanley

---
+++ app/src/main/java/com/rubanza/stanleyapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rubanza.stanleyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
